d17bc55adaffe067e09528ce68e1e588f09bbd91
Octavio Recabarren <115593701+OctaReca@users.noreply.github.com>
2023-09-26 20:27:18 -0300

    Add files via upload

---
+++ src/config/passport.config.js
@@ -0,0 +1,123 @@
+import passport from "passport";
+import local from "passport-local";
+import GitHubStrategy from "passport-github2";
+import { userModel } from "../dao/models/user.model.js";
+import { createHash, isValidPassword } from ".././utils.js";
+
+const LocalStrategy = local.Strategy;
+const initializePassport = () => {
+    passport.use(
+        "register",
+        new LocalStrategy(
+            { passReqToCallback: true, usernameField: "email" },
+            async (req, username, password, done) => {
+                const { first_name, last_name, email, age } = req.body;
+
+                try {
+                    let user = await userModel.findOne({ email: username });
+
+                    if (user) {
+                        console.log("El usuario " + email + " ya se encuentra registrado!");
+                        return done(null, false);
+                    }
+
+                    user = {
+                        first_name,
+                        last_name,
+                        email,
+                        age,
+                        password: createHash(password),
+                    };
+
+                    if (
+                        user.email == "adminCoder@coder.com" &&
+                        password === "adminCod3r123"
+                    ) {
+                        user.role = "admin";
+                    } else {
+                        user.role = "user";
+                    }
+
+                    let result = await userModel.create(user);
+                    console.log("Usuario creado con éxito:", result);
+
+                    if (result) {
+                        return done(null, result);
+                    }
+                } catch (error) {
+                    return done(error);
+                }
+            }
+        )
+    );
+
+    passport.use(
+        "login",
+        new LocalStrategy(
+            { usernameField: "email", passwordField: "password" },
+            async (username, password, done) => {
+                try {
+                    let user = await userModel.findOne({ email: username });
+                    console.log("Usuario de Passport:", user);
+
+                    if (!user) {
+                        return done(null, false, { message: "Usuario incorrecto." });
+                    }
+                    if (!isValidPassword(user, password)) {
+                        return done(null, false, { message: "Contraseña incorrecta." });
+                    }
+                    return done(null, user);
+                } catch (error) {
+                    return done(error);
+                }
+            }
+        )
+    );
+
+    passport.use(
+        "github",
+        new GitHubStrategy(
+            {
+                clientID: "Iv1.143fdc60cc16058b",
+                clientSecret: "0b906cdfc6a02bedbb58bf0577b3614f82cab86c",
+                callbackURL: "http://localhost:8080/api/sessions/githubcallback",
+            },
+            async (accessToken, refreshToken, profile, done) => {
+                try {
+                    console.log(profile);
+                    let user = await userModel.findOne({ email: profile._json.email });
+
+                    if (user) {
+                        return done(null, user);
+                    } else {
+                        let newUser = {
+                            first_name: profile._json.name,
+                            last_name: "",
+                            email: profile._json.email,
+                            age: 100,
+                            password: "",
+                            role: "user",
+                        };
+
+                        let result = await userModel.create(newUser);
+
+                        return done(null, result);
+                    }
+                } catch (error) {
+                    return done(error);
+                }
+            }
+        )
+    );
+
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        let user = await userModel.findById(id);
+        done(null, user);
+    });
+};
+
+export default initializePassport;
---
+++ src/public/register.js
@@ -0,0 +1,29 @@
+const registerUser = async () => {
+    let first_name = document.getElementById("first_name").value;
+    let last_name = document.getElementById("last_name").value;
+    let email = document.getElementById("email").value;
+    let age = document.getElementById("age").value;
+    let password = document.getElementById("password").value;
+
+    const user = { first_name, last_name, email, age, password };
+
+    const response = await fetch("/api/sessions/register", {
+        method: "POST",
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+
+    if (data.status === 'success' && data.redirect) {
+        window.location.href = data.redirect;
+    } else {
+        console.error(data.message);
+    }
+}
+
+document.getElementById("btnRegister").onclick = registerUser;
